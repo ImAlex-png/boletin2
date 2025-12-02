b3ddd5329ea9f28852660f3477fc41f5b550750c
alex1608-cell <alejc2006@gmail.com>
2025-12-02 13:14:50 +0100

    inicio boletin 2 angular

---
+++ src/app/model/direccion.ts
@@ -0,0 +1,12 @@
+export class Direccion {
+    //Atributos de la clase
+    //Los atributos privados se empiezan con ' _ '
+    private _idDireccion : number;
+    private _cod_postal : string;
+    private _ciudad : string;
+    private _municipio : string;
+    private _provincia : string;
+
+    //Constructor/constructores
+    
+}
