36c4d18f2c593728d1cc7b8d2c5f9239d459e7df
alex1608-cell <alejc2006@gmail.com>
2025-12-02 13:50:14 +0100

    creado alumno y direccion con sus getters y setters

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Direccion } from './model/direccion';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('boletin2');
+
+  public direccion : Direccion = new Direccion(1,"41520","Sevilla", "El Viso del Alcor", "Sevilla");
 }
--- src/app/model/direccion.ts
+++ src/app/model/direccion.ts
@@ -1,12 +1,60 @@
 export class Direccion {
     //Atributos de la clase
-    //Los atributos privados se empiezan con ' _ '
-    private _idDireccion : number;
-    private _cod_postal : string;
-    private _ciudad : string;
-    private _municipio : string;
-    private _provincia : string;
+    // //Los atributos privados se empiezan con ' _ '
+    // private _idDireccion: number;
+    // private _cod_postal: string;
+    // private _ciudad: string;
+    // private _municipio: string;
+    // private _provincia: string;
 
-    //Constructor/constructores
-    
+    //Constructor de tipo firma ( se hace asi 100% )
+    constructor(
+        private _idDireccion: number,
+        private _codPostal: string,
+        private _ciudad: string,
+        private _municipio: string,
+        private _provincia: string
+    ) { };
+
+    // Getters y setters
+
+    get getIdDireccion(): number {
+        return this._idDireccion;
+    }
+
+    get getCodPostal(): string {
+        return this._codPostal;
+    }
+
+    get getCiudad(): string {
+        return this._ciudad;
+    }
+
+    get getMunicipio(): string {
+        return this._municipio;
+    }
+
+    get getProvincia(): string {
+        return this._provincia;
+    }
+
+    set setIdDireccion(idDireccion: number) {
+        this._idDireccion = idDireccion;
+    }
+
+    set setCodPostal(codPostal: string) {
+        this._codPostal = codPostal;
+    }
+
+    set setCiudad(ciudad: string) {
+        this._ciudad = ciudad;
+    }
+
+    set setMunicipio(municipio: string) {
+        this._municipio = municipio;
+    }
+    set setProvincia(provincia: string) {
+        this._provincia = provincia;
+    }
+
 }
